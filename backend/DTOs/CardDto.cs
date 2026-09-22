7ecbc2a743ca10a01b4300c428e8abfba0613302
Lawrence C. <lawrencecaudle870@gmail.com>
2026-09-22 05:41:32 -0400

    added CardDto

---
+++ backend/DTOs/CardDto.cs
@@ -0,0 +1,25 @@
+﻿using backend.Entities;
+
+namespace backend.Dtos;
+
+public class CardDto
+{
+    public string Question { get; set; } = string.Empty;
+    public string Answer { get; set; } = string.Empty;
+}
+
+public class CardResponseDto
+{
+    public int Id { get; set; }
+    public int StackId { get; set; }
+    public string Question { get; set; } = string.Empty;
+    public string Answer { get; set; } = string.Empty;
+
+    public static CardResponseDto FromEntity(Card card) => new()
+    {
+        Id = card.Id,
+        StackId = card.StackId,
+        Question = card.Question,
+        Answer = card.Answer,
+    };
+}
