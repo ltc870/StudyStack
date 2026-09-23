6a6daea04e320b2c766f5255fda3233a6c12ba87
Lawrence C. <lawrencecaudle870@gmail.com>
2026-09-23 06:33:52 -0400

    Completed the card CRUD operations

--- backend/Controllers/CardsController.cs
+++ backend/Controllers/CardsController.cs
@@ -49,14 +49,14 @@ public class CardsController : ControllerBase
     
     // POST
     [HttpPost("create-card")]
-    public async Task<IActionResult> CreateCard(CardDto cardDto)
+    public async Task<IActionResult> CreateCard(CardRequestDto cardRequestDto)
     {
         // Get the userId from the token, bail if missing
         if (GetUserId() is not { } userId) return Unauthorized();
         
         // Get the right stack
         var stack = await _dbContext.Stacks.FirstOrDefaultAsync(
-            stack => stack.Id == cardDto.StackId && stack.UserId == userId);
+            stack => stack.Id == cardRequestDto.StackId && stack.UserId == userId);
         
         // Make sure the stack exists
         if (stack is null) return NotFound();
@@ -64,8 +64,8 @@ public class CardsController : ControllerBase
         // Create the card
         var card = new Card()
         {
-            Answer = cardDto.Answer,
-            Question = cardDto.Question,
+            Answer = cardRequestDto.Answer,
+            Question = cardRequestDto.Question,
             StackId = stack.Id
         };
         
@@ -73,6 +73,62 @@ public class CardsController : ControllerBase
         _dbContext.Cards.Add(card);
         await _dbContext.SaveChangesAsync();
         return CreatedAtAction(nameof(GetAllCardsByStack), new { stackId = stack.Id }, CardResponseDto.FromEntity(card));
+    }
+    
+    // Update
+    [HttpPut("update-card/{cardId}")]
+    public async Task<IActionResult> UpdateCardById(int cardId, CardUpdateDto cardUpdateDto)
+    {
+        // Get the userid from the token, bail if missing
+        if (GetUserId() is not { } userId) return Unauthorized();
+        
+        // Get the card by filtering on the stack and making sure it belongs to the correct user
+        var card = await _dbContext.Cards
+            .FirstOrDefaultAsync(card =>  card.Id == cardId && card.Stack.UserId == userId);
+        
+        // Make sure the card is found
+        if (card is null) return NotFound();
+        
+        // Check the cardUpdateDto. If or empty null, then leave the question as is, if not null, then 
+        // assign cardUpdateDto.Question to card.Question
+        if (cardUpdateDto.Question != null)
+        {
+            card.Question = cardUpdateDto.Question;
+        }
+        
+        // // Check the cardUpdateDto. If null or empty, then leave the answer as is, if not null, then 
+        // assign cardUpdateDto.Question to card.Answer
+        if (cardUpdateDto.Answer != null)
+        {
+            card.Answer = cardUpdateDto.Answer;
+        }
+        
+        // Save changes made
+        await _dbContext.SaveChangesAsync();
+
+        // Return the updated card
+        return Ok(CardResponseDto.FromEntity(card));
+    }
+    
+    // DELETE
+    [HttpDelete("delete-card/{cardId}")]
+    public async Task<IActionResult> DeleteCardById(int cardId)
+    {
+        // Get the userId
+        if (GetUserId() is not { } userId) return Unauthorized();
+        
+        // Get the card
+        var card = await _dbContext.Cards
+            .FirstOrDefaultAsync(card => card.Id == cardId && card.Stack.UserId == userId);
+        
+        // Make sure the card exists
+        if (card is null) return NotFound();
+        
+        // Delete the card
+        _dbContext.Remove(card);
+        await _dbContext.SaveChangesAsync();
+
+        return NoContent();
     }
     
     // Private Methods
--- backend/DTOs/CardDto.cs
+++ backend/DTOs/CardDto.cs
@@ -2,11 +2,17 @@
 
 namespace backend.Dtos;
 
-public class CardDto
+public class CardRequestDto
 {
     public int StackId { get; set; }
     public string Question { get; set; } = string.Empty;
     public string Answer { get; set; } = string.Empty;
+}
+
+public class CardUpdateDto
+{
+    public string? Question { get; set; }
+    public string? Answer { get; set; }
 }
 
 public class CardResponseDto
