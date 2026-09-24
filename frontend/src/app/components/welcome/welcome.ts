ba4e6406f6b1d717fa6211674dd5946ca57c4c4c
Lawrence C. <lawrencecaudle870@gmail.com>
2026-09-24 07:58:15 -0400

    started working on welcome screen

---
+++ frontend/src/app/components/welcome/welcome.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-welcome',
+  styleUrl: './welcome.scss',
+  templateUrl: './welcome.html',
+})
+export class Welcome {}
