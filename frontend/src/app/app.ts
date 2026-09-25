08645039dc8712985be2bd241577a10ac8706bfb
Lawrence C. <lawrencecaudle870@gmail.com>
2026-09-25 07:14:48 -0400

    started working on html/css

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Welcome } from './components/welcome/welcome';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Welcome],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
